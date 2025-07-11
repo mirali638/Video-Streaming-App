98fec23ecd61b04fe03068210ce8cf085496cc49
Mirali Jivani <miralijivani632@gmail.com>
2025-07-11 14:55:36 +0530

    file upload setup

--- src/app.js
+++ src/app.js
@@ -4,18 +4,32 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true, // Allow cookies to be sent with requests
-}));
+  })
+);
 app.use(cookieParser());
-app.use(express.json({
+app.use(
+  express.json({
     limit: "16kb", // Increase the limit to 16kb
-}));
-app.use(express.urlencoded({
+  })
+);
+app.use(
+  express.urlencoded({
     limit: "16kb", // Increase the limit to 16kb
     extended: true,
-}));
+  })
+);
 app.use(express.static("public")); // Serve static files from the 'public' directory
 
-export { app };
+//routes import
+import userRouter from "./routes/user.routes.js";
+
+//routes declaration
+//http://localhost:8000/api/v1/users/register
+//http://localhost:8000/api/v1/users/login
+app.use("/api/v1/users", userRouter);
+
+export default app;
